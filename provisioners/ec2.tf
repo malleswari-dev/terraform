e5ae75b29a32942951f22cce0e262fa54109e945
malleswari <maddikirimalleswari4@gmail.com>
2025-11-09 18:09:17 +0530

    message

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -10,12 +10,36 @@ resource "aws_instance" "terraform" {
   provisioner "local-exec"{
     command = "echo ${self.private_ip} > inventory"
     on_failure = continue
-    }
+  }
 
   provisioner "local-exec"{
-      command = "echo Instance is destroyed"
-      when    = destroy
-    }  
+    command = "echo Instance is destroyed"
+    when    = destroy
+  }
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "sudo dnf install nginx -y",
+        "sudo systemctl start nginx"
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "sudo systemctl stop nginx",
+        "echo 'successfully stoped nginx server'"
+    ]
+    when = destroy
+  }  
+
+
 }
 
 resource "aws_security_group" "allow-all" {
